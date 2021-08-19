5cb5f9b7d437907f0ec5595b0673322d73fcb45f
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-19 10:47:20 -0700

    separated eliza from variables_dot_notation

--- eliza_project/main.go
+++ eliza_project/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/variables_dot_notation/doctor"
+	"os"
 )
 
 func main() {
-	var whatToSay string
+	reader := bufio.NewReader(os.Stdin)
 
 	whatToSay := doctor.Intro()
 
 	fmt.Println(whatToSay)
+
 }
