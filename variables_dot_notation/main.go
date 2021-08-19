e6f4425bde12716e0eba2daaecb819c1ef57ad0c
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-19 09:41:40 -0700

    begin variables and dot notation lesson

---
+++ variables_dot_notation/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var whatToSay string
+	whatToSay = "Hello, world again!"
+
+	sayHelloWorld(whatToSay)
+}
+
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
+}
