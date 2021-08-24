fbe7fe14c3363021f2c5365cdbedd3caf1919ae5
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-23 22:52:29 -0700

    scope challenge [2.17] folder and proposed solution

--- section2/packagetwo/package.go
+++ section2/packagetwo/package.go
@@ -1,1 +1,9 @@
 package packagetwo
+
+import "fmt"
+
+var PackageVar = "[package level variable in package pagetwo]"
+
+func PrintMe(myVar, blockVar string) {
+	fmt.Println(myVar, blockVar, PackageVar)
+}
--- section2/scope_challenge_2.17/main.go
+++ section2/scope_challenge_2.17/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"myapp/section2/packagetwo"
+)
+
+var myVar = "[package level variable in package main]"
+
 func main() {
+	blockVar := "[block level variable in package main, func main]"
+	packagetwo.PrintMe(myVar, blockVar)
 
 }
