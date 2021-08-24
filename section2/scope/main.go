15a952f317eb68ec12eec3225fc560a01e9ab94d
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-23 21:12:39 -0700

    block level variables

--- section2/scope/main.go
+++ section2/scope/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var one = "One"
+
+	fmt.Println(one)
+
+	myFunc()
+}
+
+func myFunc() {
+	var one = "the number one"
+
+	fmt.Println(one)
 }
