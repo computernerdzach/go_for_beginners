c67ab30d22bb5fd5f2e72281a78065a9428c03a2
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-25 10:19:16 -0700

    initialize section 4 with built-in-types project

---
+++ section4/built-in-types/main.go
@@ -0,0 +1,13 @@
+package main
+
+// basic types (numbers, strings, booleans)
+
+// aggregate types (array, struct)
+
+// reference types (pointers, slices, maps, functions, channels)
+
+// interface type
+
+func main() {
+
+}
