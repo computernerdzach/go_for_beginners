b65fdeac93b66ae6c77c70b484e0ca4f128c1a47
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-24 18:15:24 -0700

    created custom variable type utilzing [type] and [struct]. Created function to loop until a favorite number is entered. Improved string interpolation in [main()]

--- section3/string_interpolation/main.go
+++ section3/string_interpolation/main.go
@@ -10,13 +10,25 @@ import (
 
 var reader *bufio.Reader
 
+type User struct {
+	UserName       string
+	Age            int
+	FavoriteNumber float64
+}
+
 func main() {
 	reader = bufio.NewReader(os.Stdin)
-	userName := readString("What is your name?")
 
-	age := readInt("How old are you?")
+	var user User
 
-	fmt.Printf("Your name is %s. You are %d years old.\n", userName, age)
+	user.UserName = readString("What is your name?")
+	user.Age = readInt("How old are you?")
+	user.FavoriteNumber = readFloat("What is your favorite number?")
+
+	fmt.Printf("Your name is %s. You are %d years old. Your favorite number is %.2f\n",
+		user.UserName,
+		user.Age,
+		user.FavoriteNumber)
 }
 
 func prompt() {
@@ -51,6 +63,24 @@ func readInt(s string) int {
 		num, err := strconv.Atoi(userInput)
 		if err != nil {
 			fmt.Println("Please enter a whole number.")
+		} else {
+			return num
+		}
+	}
+}
+
+func readFloat(s string) float64 {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.ParseFloat(userInput, 64)
+		if err != nil {
+			fmt.Println("Please enter a number.")
 		} else {
 			return num
 		}
