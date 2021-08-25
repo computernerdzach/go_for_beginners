64e2edbc19afc956b5cf01da4e714c1b2f57e4d4
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-25 10:28:37 -0700

    all basic types added

--- section4/built-in-types/main.go
+++ section4/built-in-types/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "log"
+
 // basic types (numbers, strings, booleans)
+var myInt int
+var myUint uint
+var myFloat float32
+var myFloat64 float64
 
 // aggregate types (array, struct)
 
@@ -9,5 +15,23 @@ package main
 // interface type
 
 func main() {
+	myInt = 10
+	myUint = 20
+
+	myFloat = 10.1
+	myFloat64 = 100.1
+
+	log.Println(myInt, myUint, myFloat, myFloat64)
+
+	myString := "Zach"
+
+	log.Println(myString)
+
+	myString = "John"
+
+	var myBool = true
+	myBool = false
+
+	log.Println(myBool)
 
 }
