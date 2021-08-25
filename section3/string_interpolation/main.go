7467558e6e8a04f5d1c8141ad6f3314166645360
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-24 17:07:15 -0700

    loops until correct type of number is entered for age

--- section3/string_interpolation/main.go
+++ section3/string_interpolation/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,7 +14,9 @@ func main() {
 	reader = bufio.NewReader(os.Stdin)
 	userName := readString("What is your name?")
 
-	fmt.Println("Your name is", userName)
+	age := readInt("How old are you?")
+
+	fmt.Println("Your name is", userName, "and you are", age, "years old.")
 }
 
 func prompt() {
@@ -29,4 +32,22 @@ func readString(s string) string {
 	userInput = strings.Replace(userInput, "\n", "", -1)
 
 	return userInput
+}
+
+func readInt(s string) int {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Please enter a whole number.")
+		} else {
+			return num
+		}
+	}
 }
