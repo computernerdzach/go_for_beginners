b43e996bd8405ce7cc250f58dd4498b63ba89a4b
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-25 11:37:48 -0700

    channels

--- section4/built-in-types/main.go
+++ section4/built-in-types/main.go
@@ -1,52 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// reference types (pointers, slices, maps, functions, channels)
+	"github.com/eiannone/keyboard"
+)
 
-// interface type
-
-type Animal struct {
-	Name         string
-	Sound        string
-	NumberOfLegs int
-}
-
-// reciever points to type above, like a class function in python
-func (a *Animal) Says() {
-	fmt.Printf("A %s says %s", a.Name, a.Sound)
-	fmt.Println()
-}
-
-func (a *Animal) HowManyLegs() {
-	fmt.Printf("A %s has %d legs", a.Name, a.NumberOfLegs)
-	fmt.Println()
-}
+var keyPressChan chan rune
 
 func main() {
-	var dog Animal
-	dog.Name = "dog"
-	dog.Sound = "woof"
-	dog.NumberOfLegs = 4
-	dog.Says()
+	keyPressChan = make(chan rune)
 
-	cat := Animal{
-		Name:         "cat",
-		Sound:        "meow",
-		NumberOfLegs: 4,
+	go listenForKeyPress()
+
+	fmt.Println("Press any key, or q to quit")
+	_ = keyboard.Open()
+	defer func() {
+		keyboard.Close()
+	}()
+
+	for {
+		char, _, _ := keyboard.GetSingleKey()
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		keyPressChan <- char
 	}
-
-	cat.Says()
-	cat.HowManyLegs()
 }
 
-// variatic function
-func sumMany(nums ...int) int {
-	total := 0
-
-	for _, x := range nums {
-		total = total + x
+func listenForKeyPress() {
+	for {
+		key := <-keyPressChan
+		fmt.Println("You pressed", string(key))
 	}
-
-	return total
 }
