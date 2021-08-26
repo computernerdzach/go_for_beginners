63e3fd4d2f3b499561a5d9402f5531bcfd792a67
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-08-26 00:09:27 -0700

    initialize exported project and established a staff package that is accessed from main.go

---
+++ section4/exported/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"myapp/section4/exported/staff"
+)
+
+var employees = []staff.Employee{
+	{FirstName: "John", LastName: "Smith", Salary: 30000, FullTime: true},
+	{FirstName: "Sally", LastName: "Jones", Salary: 50000, FullTime: true},
+	{FirstName: "Mark", LastName: "Smithers", Salary: 60000, FullTime: true},
+	{FirstName: "Alan", LastName: "Resnick", Salary: 15000, FullTime: false},
+	{FirstName: "Margaret", LastName: "Carter", Salary: 100000, FullTime: false},
+}
+
+func main() {
+	myStaff := staff.Office{
+		AllStaff: employees,
+	}
+
+	myStaff.All()
+
+	log.Println(myStaff.All())
+
+}
