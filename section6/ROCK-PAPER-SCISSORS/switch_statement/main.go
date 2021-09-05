f32835255bfb861d44d8e337e0734e834349d260
Zachary Theilen <45415247+computernerdzach@users.noreply.github.com>
2021-09-05 13:12:53 -0700

    fixed printout and index tracking

--- section6/ROCK-PAPER-SCISSORS/switch_statement/main.go
+++ section6/ROCK-PAPER-SCISSORS/switch_statement/main.go
@@ -21,14 +21,25 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	playerChoice := ""
 	playerValue := -1
-
-	computerValue := rand.Intn(2)
+	playerWins := 0
+	computerWins := 0
 
 	reader := bufio.NewReader(os.Stdin)
 
 	clearScreen()
 
+	fmt.Println("Rock, Paper, & Scissors")
+	fmt.Println("-----------------------")
+	fmt.Println("Game is played for three rounds, and best of three wins the game. Good luck!")
+	fmt.Println()
+	fmt.Println("-------")
+
 	for i := 0; i < 3; i++ {
+
+		computerValue := rand.Intn(2)
+
+		fmt.Println("Round", i+1)
+		fmt.Println("-------")
 		fmt.Print("Please enter rock, paper, or scissors ->")
 		playerChoice, _ = reader.ReadString('\n')
 		playerChoice = strings.Replace(playerChoice, "\r\n", "", -1)
@@ -52,34 +63,57 @@ func main() {
 		default:
 		}
 
-		fmt.Println()
-
 		if playerValue == computerValue {
 			fmt.Println("It's a draw")
 		} else {
 			switch playerValue {
 			case ROCK:
 				if computerValue == PAPER {
-					fmt.Println("Computer wins!")
+					fmt.Println("--Computer wins!--")
+					computerWins++
 				} else {
-					fmt.Println("Player wins!")
+					fmt.Println("--Player wins!--")
+					playerWins++
 				}
 			case PAPER:
 				if computerValue == SCISSORS {
-					fmt.Println("Computer wins!")
+					fmt.Println("--Computer wins!--")
+					computerWins++
 				} else {
-					fmt.Println("Player wins!")
+					fmt.Println("--Player wins!--")
+					playerWins++
 				}
 			case SCISSORS:
 				if computerValue == ROCK {
-					fmt.Println("Computer wins!")
+					fmt.Println("--Computer wins!--")
+					computerWins++
 				} else {
-					fmt.Println("Player wins!")
+					fmt.Println("--Player wins!--")
+					playerWins++
 				}
 			default:
-				fmt.Println("Invalid choice!")
+				fmt.Println("--Invalid choice!--")
 			}
+			fmt.Println()
+			if i < 2 {
+				fmt.Println("-------")
+			}
+
 		}
+	}
+
+	if playerWins < computerWins {
+		fmt.Println("*********************")
+		fmt.Println("*Winner is Computer!*")
+		fmt.Println("*********************")
+	} else if playerWins > computerWins {
+		fmt.Println("*******************")
+		fmt.Println("*Winner is Player!*")
+		fmt.Println("*******************")
+	} else {
+		fmt.Println("*****************")
+		fmt.Println("*Game is a draw!*")
+		fmt.Println("*****************")
 	}
 }
 
